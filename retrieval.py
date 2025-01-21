d691177622803ce15e65d933e257ec2b7481797d
Saikishen <128302556+Saiikishen@users.noreply.github.com>
2025-01-21 21:14:23 +0530

    Update retrieval.py

--- retrieval.py
+++ retrieval.py
@@ -12,20 +12,16 @@ load_dotenv()
 if __name__ == "__main__":
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
-    query = "Who constructed the Taj Mahal?"
+    query = "Who made the Taj Mahal?"
 
     prompt_template = PromptTemplate(input_variables=[], template=query)
 
     vectorstore = PineconeVectorStore(index_name="mokesh", embedding=embeddings)
 
-    # prompt for retrieving
     prompt = hub.pull('langchain-ai/retrieval-qa-chat')
 
-    # create_stuff_documents_chain- This chain takes list of documents and formats them into a single document and then passes it to
-    # the LLM for further processing.
     combined_docs_chain = create_stuff_documents_chain(llm, prompt)
 
-    # create_retriever_chain- This chain takes a query, passes it to the retriever, retrieves a list of documents, and then passes
     retriever_chain = create_retrieval_chain(retriever=vectorstore.as_retriever(), combine_docs_chain=combined_docs_chain)
 
     result = retriever_chain.invoke({ "input": query })
