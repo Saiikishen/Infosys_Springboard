e6e50466732945550334a5ba233717aa010c35fc
Saikishen <128302556+Saiikishen@users.noreply.github.com>
2025-01-21 21:00:39 +0530

    Update ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -9,23 +9,19 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 if __name__ == "__main__":
 
-    # 1. Loading Documents
     print("Loading Documents...")
     loader = TextLoader('./information.txt', encoding='utf-8')
-    document = loader.load() # loads the documents with metadata
+    document = loader.load() #
     print(f"Loaded {len(document)} documents")
 
-    # 2. Splitting Documents
     print("Splitting Documents...")
     splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200) 
     split_documents = splitter.split_documents(document)
     print(f"Split {len(document)} documents into {len(split_documents)} chunks")
 
-    # 3. Embedding Documents
     print("Started Embedding Documents...")
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-    # 4. Inserting Documents into VectorDB
     print("Inserting Documents into VectorDB...")
     vector_db = PineconeVectorStore.from_documents(split_documents, embeddings, index_name="mokesh")
     print(f"Inserted {len(split_documents)} documents into VectorDB")
